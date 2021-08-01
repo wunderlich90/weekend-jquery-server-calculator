e1ba281a78a88d5aa581ad09444fb01af571c527
Jeremy Cady <jcady90@icloud.com>
2021-08-01 14:34:30 -0500

    server functioning, wrote a fucntion on client side to handle math operator button click events

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,44 +1,103 @@
 $(document).ready(onReady);
 
+// Declare global object
+let newCalculation = {
+    inputOne: $('#inputOne').val(),
+    mathOperatorInput: $(this).attr('id'),
+    inputTwo: $('#inputTwo').val(),
+};
+
+
 function onReady() {
     console.log('Ready!');
 
     // On page load get calculations from server
     getCalculations();
 
-    console.log('add button', $('#addButton'));
-    $('#addButton').on('click, ')
-    
-    
+    console.log('equals button', $('#equalsButton'));
+
+    $('#equalsButton').on('click', addCalculation);
+
+    $('.calcButtons').on('click', mathOperators);
+
+       
 }
+
+ function mathOperators () {
+     let mathOperator = $(this).attr('id')
+     console.log(mathOperator);
+     newCalculation.mathOperatorInput = mathOperator;
+ }
+
+
+
+ function addCalculation() {
+     let inputOne = $('#inputOne').val();
+     let mathOperator = $(this).attr('id');
+     let inputTwo = $('#inputTwo').val();
+    newCalculation.inputOne = inputOne;
+    newCalculation.inputTwo = inputTwo;
+    newCalculation.mathOperatorInput = mathOperator;
+
+     console.log('inside addCalculation');
+
+     console.log('newCalculation is', newCalculation);
+    
+     // POST calculation to /calculations
+     $.ajax({
+         method: 'POST',
+         url: '/calculations',
+         // send quote data to the server
+         data: newCalculation
+     })
+        .then((response) => {
+         console.log('POST /calculations', response);
+         // Refresh data from the server
+         getCalculations();  
+      })
+        .catch((error) => {
+          console.log('POST /calculations failed!', error);
+          // Render error to the DOM
+          $('body').append(`
+              <h2>
+                  Failed to save calculation! Please make sure all input fileds contain data.
+              </h2>
+          `);
+        
+     });
+
+ }
+
+
 
 function getCalculations() {
-    // Make HTTP request to server to get /calculations endpoint
-    //
-    // Call the function in server.js
-    $.ajax({
-        //Tell AJAX what endpoint to hit
-        method: 'GET',
-        url: '/calculations'
-    }).then((response) => {
-            console.log('GET /calculations response', response);
+     // Make HTTP request to server to get /calculations endpoint
+     //
+     // Call the function in server.js
+     $.ajax({
+         //Tell AJAX what endpoint to hit
+         method: 'GET',
+         url: '/calculations'
+     })
+        .then((response) => {
+             console.log('GET /calculations response', response);
             
-            // Render calculations with jQuery
-            let calculationsList = $('#calculations');
-            console.log('calculations list element', calculationsList);
+             // Render calculations with jQuery
+             let calculationsList = $('#calculations');
+             console.log('calculations list element', calculationsList);
 
-            // loop thropugh the array of calculations
-            calculationsList.empty();
-            for(let i=0; i<response.length; i++) {
-                let calculation = response[i];
-                // Render <li> with each calculation
-                calculationsList.append(`
-                    <li>
-                        ${calculation.inputOne}
-                    </li>
-                `);
+             // loop thropugh the array of calculations
+             calculationsList.empty();
+             for(let i=0; i<response.length; i++) {
+                 let calculation = response[i];
+                 // Render <li> with each calculation
+                 calculationsList.append(`
+                     <li>
+                         ${calculation.inputOne}  ${calculation.inputTwo} = ${calculation.total}
+                     </li>
+                 `);
 
-            }
+             }
              
-        });
-}
+         });
+ }
--- server/server.js
+++ server/server.js
@@ -6,32 +6,11 @@ const express = require('express');
 // Load the body-parser module
 const bodyParser = require('body-parser');
 
-// Create app (server)
+//Create app (server)
 const app = express();
 
-// Data
-const calculationsArray = [
-    {
-        inputOne: 2,
-        inputTwo: 3,
-        total: 5
-    },
-    {
-        inputOne: 6,
-        inputTwo: 3,
-        total: 3
-    },
-    {
-        inputOne: 5,
-        inputTwo: 4,
-        total: 20
-    },
-    {
-        inputOne: 6,
-        inputTwo: 3,
-        total: 2
-    }
-]
+ // Data
+ const calculationsArray = [];
 
 // Tell express where to find public files
 app.use(express.static('./server/public'));
@@ -40,55 +19,60 @@ app.use(express.static('./server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
-// Listen for requests coming from a specific url
-// http://localhost:5000/calculations
-app.get('/calculations', function(req, res) {
-    console.log('Ready to send back some calculations');
-    console.log('request.route.path is', req.route.path);
+ // Listen for requests coming from a specific url
+ // http://localhost:5000/calculations
+ app.get('/calculations', function(req, res) {
+     console.log('Ready to send back some calculations');
+     console.log('request.route.path is', req.route.path);
 
-    // Send data back to the client
-    res.send(calculationsArray);
+     // Send data back to the client
+     res.send(calculationsArray);
     
-});
+ });
 
-// POST /calculations
-app.post('/calculations', function (req, res) {
-    console.log('New calculation received');
+ // GET /first-calculation
+ app.get('/first-calculation', (req,res) => {
+     res.send(calculationsArray[0]);
+ });
 
-    //Bodyparser gives us req.body
-    console.log('req.body', req.body);
-    let newCalculation = req.body;
-    calculations.push(newCalculation);
+ // POST /calculations
+ app.post('/calculations', (req, res) => {
+     console.log('New calculation received');
 
-    // In the event of empty input fields send an error message
-    if (!newCalculation.numberOne || !newCalculation.numberTwo) {
-        // Error 400 w/message
-        res.status(400).send( {
-            message: 'Missing required field'
-        });
-        // End the function 
-        return;
-    }
+     //Bodyparser gives us req.body
+     console.log('req.body', req.body);
+     let newCalculation = req.body;
+     calculationsArray.push(newCalculation);
 
-    res.sendStatus(200);
+     // In the event of empty input fields send an error message
+     if (!newCalculation.inputOne || !newCalculation.inputTwo) {
+         // Error 400 w/message
+         res.status(400).send( {
+             message: 'Missing required field'
+         });
+         // End the function 
+         return;
+     }
+
+     res.sendStatus(200);
     
-});
+ });
 
-// Listen for requests
-const port = 5000;
-app.listen(port, function(){
-    console.log('App is up and running on localhost:5000');
+ // Listen for requests
+ const port = 5000;
+ app.listen(port, function(){
+     console.log('App is up and running on localhost:5000');
     
-});
+ });
 
-function calculator() {
+ function calculator() {
 
-}
+ }
 
-function add(inputOne, inputTwo) {
-    let sumTotal = inputOne = inputTwo;
-    return sumTotal;
-}
+ function add(inputOne, inputTwo) {
+     let sumTotal = inputOne = inputTwo;
+     return sumTotal;
+ }
 
 
 
